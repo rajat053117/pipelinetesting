076c84f6c148e0e66145063dcea6840d6312047e
Rajat Verma <46910206+rajat2911@users.noreply.github.com>
2024-11-05 08:13:45 +0530

    Update fetch-project.sh

--- fetch-project.sh
+++ fetch-project.sh
@@ -1,5 +1,32 @@
-#!/bin/bash
-
-set -euo pipefail
-
-source_location=$(buildkite-agent meta-data get source_location)
+# Check the selected source location
+if [[ "$source_location" == "legacy-quorbit" ]]; then
+  echo '  - block: "Environment Options"
+      fields:
+        - select: "Environment Name"
+          key: "source_env"
+          hint: Select the Environment where redis cache needs to be flushed
+          required: true
+          options:
+            - label: "Staging"
+              value: "staging"
+            - label: "Preprod"
+              value: "preprod"
+            - label: "Prod"
+              value: "prod"
+            - label: "Prod-US"
+              value: "prod-us"' 
+else
+  echo '  - block: "Environment Options"
+      fields:
+        - select: "Environment Name"
+          key: "source_env"
+          hint: Select the Environment where redis cache needs to be flushed
+          required: true
+          options:
+            - label: "Staging-US"
+              value: "staging-us"
+            - label: "Prod-US"
+              value: "prod-us"
+            - label: "Prod-EU"
+              value: "prod-eu"'  
+fi
